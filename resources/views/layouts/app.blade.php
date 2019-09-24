7474b582b5d3e722f89ef8aad98913145779fd70
Midiel Rodriguez <midiel305@gmail.com>
2019-09-24 14:27:59 -0400

    Users roles implementation and seed the database with test accounts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,7 +33,9 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
+                        <li class="nav-item">
+                            <a href="{{ route('admin.users.index') }}">Manage Users</a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
@@ -77,6 +79,7 @@
         </nav>
 
         <main class="py-4">
+            @include('partials.alerts')
             @yield('content')
         </main>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 
 Route::namespace('Admin')->prefix('admin')->name('admin.')->group(function() {
-    
-    Route::resource('/users', 'UsersController');
+
+    Route::resource('/users', 'UsersController')->middleware(['auth', 'auth.admin']);
 });
+
