beb1850464be2dfc041b12c5a4d8600039af6bdd
Felix Diaz <fdiaz3000@gmail.com>
2019-09-30 22:41:10 -0400

    Create Officer Model

--- database/migrations/2019_10_01_023941_create_officers_table.php
+++ database/migrations/2019_10_01_023941_create_officers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateSpecialtiesTable extends Migration
+class CreateOfficersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,8 @@ class CreateSpecialtiesTable extends Migration
      */
     public function up()
     {
-        Schema::create('specialties', function (Blueprint $table) {
+        Schema::create('officers', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('specialty_name');
             $table->timestamps();
         });
     }
@@ -27,6 +26,6 @@ class CreateSpecialtiesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('specialties');
+        Schema::dropIfExists('officers');
     }
 }
