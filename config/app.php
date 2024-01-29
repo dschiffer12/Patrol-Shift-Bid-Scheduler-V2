d79172675278cd9053556f3e5b699c0cddd4bf21
Dustin Schiffer <73139931+dschiffer12@users.noreply.github.com>
2024-01-29 08:40:01 -0700

    Update app.php

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'IAFF Shift Bid Portal'),
 
     /*
     |--------------------------------------------------------------------------
